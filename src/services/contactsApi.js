3906f340d26ea4cfd21d1c66e9c5bc31ce21caac
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-10-16 00:05:32 +0200

    a
    a

--- src/services/contactsApi.js
+++ src/services/contactsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const contactsApi = createApi({
   reducerPath: 'contacts',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://634b2ae633bb42dca4116d95.mockapi.io/api/v1/',    
+    baseUrl: 'https://634b2ae633bb42dca4116d95.mockapi.io//api/v1/',    
   }),
   tagTypes: ['Contacts'],
   endpoints: builder => ({
